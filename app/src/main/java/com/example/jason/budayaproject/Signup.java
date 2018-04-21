2d87db14f2c8a8db32ae3dbd074da83c0c6afb74
Jason <jasonpratama_16@kharisma.ac.id>
2018-04-21 14:08:59 +0800

    test2

---
+++ app/src/main/java/com/example/jason/budayaproject/Signup.java
@@ -0,0 +1,24 @@
+package com.example.jason.budayaproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Signup extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
