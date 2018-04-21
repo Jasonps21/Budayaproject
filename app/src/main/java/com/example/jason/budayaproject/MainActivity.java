b21a10b367e5956c4d6b441a1b4a141f468b9d2b
Davin <davinirawan18@gmail.com>
2018-04-21 20:01:07 +0800

    Main Activity with sidebar navigation

--- app/src/main/java/com/example/jason/budayaproject/MainActivity.java
+++ app/src/main/java/com/example/jason/budayaproject/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.jason.budayaproject;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -12,16 +19,80 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView rv_wisata;
     WisataAdapter wisataAdapter;
-
+    //Mendefinisikan variabel
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         rv_wisata = (RecyclerView) findViewById(R.id.rv_wisata);
         rv_wisata.setItemAnimator(new DefaultItemAnimator());
         //List<wisata> movies = jsonUtils.getAllMovies(s);
         //wisataAdapter = new WisataAdapter(movies,MainActivity.this);
         rv_wisata.setAdapter(wisataAdapter);
+
+
+        // Menginisiasi Toolbar dan mensetting sebagai actionbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Menginisiasi  NavigationView
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        //Mengatur Navigasi View Item yang akan dipanggil untuk menangani item klik menu navigasi
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                //Memeriksa apakah item tersebut dalam keadaan dicek  atau tidak,
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+                //Menutup  drawer item klik
+                drawerLayout.closeDrawers();
+                //Memeriksa untuk melihat item yang akan dilklik dan melalukan aksi
+                switch (menuItem.getItemId()){
+                    // pilihan menu item navigasi akan menampilkan pesan toast klik kalian bisa menggantinya
+                    //dengan intent activity
+                    case R.id.navigation1:
+                        Toast.makeText(getApplicationContext(), "Beranda Telah Dipilih", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.navigation2:
+                        Toast.makeText(getApplicationContext(),"Profil Telah Dipilih",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.navigation3:
+                        Toast.makeText(getApplicationContext(),"Daftar Telah Dipilih",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.navigation4:
+                        Toast.makeText(getApplicationContext(),"Setting telah dipilih",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.navigation5:
+                        Toast.makeText(getApplicationContext(),"About telah dipilih",Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(),"Kesalahan Terjadi ",Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+            }
+        });
+        // Menginisasi Drawer Layout dan ActionBarToggle
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Kode di sini akan merespons setelah drawer menutup disini kita biarkan kosong
+                super.onDrawerClosed(drawerView);
+            }
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                //  Kode di sini akan merespons setelah drawer terbuka disini kita biarkan kosong
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        //Mensetting actionbarToggle untuk drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        //memanggil synstate
+        actionBarDrawerToggle.syncState();
     }
 }
